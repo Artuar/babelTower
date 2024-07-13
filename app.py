b3b8e473cb27859d78593bccb55d0c6fa188341a
artur <artuar1990@gmail.com>
2024-07-13 23:37:31 +0300

    # load and save lang model

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
+import os
 import whisper
 from transformers import MarianMTModel, MarianTokenizer
 import soundfile as sf
 import torch
 
+# Функція для завантаження моделі Whisper
+def load_whisper_model(model_name="base"):
+    return whisper.load_model(model_name)
+
+# Функція для завантаження або завантаження та збереження моделі перекладу
+def load_or_download_translation_model(model_name='Helsinki-NLP/opus-mt-en-uk', local_dir='local_model'):
+    if os.path.exists(local_dir):
+        tokenizer = MarianTokenizer.from_pretrained(local_dir)
+        translation_model = MarianMTModel.from_pretrained(local_dir)
+    else:
+        tokenizer = MarianTokenizer.from_pretrained(model_name)
+        translation_model = MarianMTModel.from_pretrained(model_name)
+        tokenizer.save_pretrained(local_dir)
+        translation_model.save_pretrained(local_dir)
+    return tokenizer, translation_model
+
+# Функція для завантаження або завантаження та збереження моделі Silero
+def load_or_download_silero_model(repo_or_dir='snakers4/silero-models', model_name='silero_tts', language='ua', speaker='v4_ua'):
+    return torch.hub.load(repo_or_dir=repo_or_dir, model=model_name, language=language, speaker=speaker)
+
 # Завантаження моделі Whisper
-whisper_model = whisper.load_model("base")
+whisper_model = load_whisper_model("base")
 
 # Завантаження аудіо файлу та підготовка його для розпізнавання
+print("Loading original audio")
 audio = whisper.load_audio("audio/audio.mp3")
 audio = whisper.pad_or_trim(audio)
 
@@ -26,9 +48,7 @@ recognized_text = result.text
 print(f"Recognized text: {recognized_text}")
 
 # Завантаження перекладацької моделі та токенізатора
-model_name = 'Helsinki-NLP/opus-mt-en-uk'  # замініть на потрібні моделі для вашої мови
-tokenizer = MarianTokenizer.from_pretrained(model_name)
-translation_model = MarianMTModel.from_pretrained(model_name)
+tokenizer, translation_model = load_or_download_translation_model()
 
 # Функція для перекладу тексту
 def translate_text(text, tokenizer, model):
@@ -41,16 +61,14 @@ def translate_text(text, tokenizer, model):
 translated_text = translate_text(recognized_text, tokenizer, translation_model)
 print(f"Translated text: {translated_text}")
 
-model, example_text = torch.hub.load(repo_or_dir='snakers4/silero-models',
-                                     model='silero_tts',
-                                     language='ua',
-                                     speaker='v4_ua')
+model, example_text = load_or_download_silero_model()
 
 device = torch.device('cpu')
 model.to(device)
 audio = model.apply_tts(text=translated_text,
                         speaker='mykyta',
                         sample_rate=48000)
+print("Save translated audio")
 
 # Збереження аудіо у файл
 sf.write('audio/translated_audio.wav', audio, 48000)
