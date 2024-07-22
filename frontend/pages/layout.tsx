f51740d40bd759d3a4a587ce965594f39f9d3e46
artur <artuar1990@gmail.com>
2024-07-23 00:53:24 +0300

    # fix page height

--- frontend/pages/layout.tsx
+++ frontend/pages/layout.tsx
@@ -1,6 +1,7 @@
 import {
   Toolbar,
   Container,
+  Box,
 } from '@mui/material';
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
@@ -11,18 +12,22 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <>
+    <Box
+      display="flex"
+      flexDirection="column"
+      minHeight="100vh"
+    >
       <Header />
 
       {/* Add top padding to the main container to avoid content being hidden behind the AppBar */}
       <Toolbar />
 
-      <Container>
+      <Container style={{ flexGrow: 1 }}>
         {children}
       </Container>
 
       <Footer />
-    </>
+    </Box>
   );
 };
 
